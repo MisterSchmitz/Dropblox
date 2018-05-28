a0e35145d4fdaf7414a11a10b9beb9c7c67b557f
Mike Schmitz <misterschmitz@gmail.com>
2018-05-28 16:07:27 -0700

    basic blockstore impl

--- java/src/main/java/surfstore/Client.java
+++ java/src/main/java/surfstore/Client.java
@@ -1,8 +1,11 @@
 package surfstore;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+
+import com.google.protobuf.ByteString;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -11,7 +14,8 @@ import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 import surfstore.SurfStoreBasic.Empty;
-
+import surfstore.SurfStoreBasic.Block;
+import surfstore.SurfStoreBasic.Block.Builder;
 
 public final class Client {
     private static final Logger logger = Logger.getLogger(Client.class.getName());
@@ -40,15 +44,53 @@ public final class Client {
         metadataChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
         blockChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
-    
+
+    private void ensure(boolean b) {
+        if (b == false) {
+            throw new RuntimeException("Assertion failed!");
+        }
+    }
+
+    private static Block stringToBlock(String s) {
+        Builder builder = Block.newBuilder();
+
+        try {
+            builder.setData(ByteString.copyFrom(s, "UTF-8"));
+//        builder.setData(ByteString.copyFrom(byte[]));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        builder.setHash(HashUtils.sha256(s));
+
+        return builder.build(); // turns the Builder into a Block
+    }
+
 	private void go() {
-		metadataStub.ping(Empty.newBuilder().build());
-        logger.info("Successfully pinged the Metadata server");
+//		metadataStub.ping(Empty.newBuilder().build());
+//        logger.info("Successfully pinged the Metadata server");
         
         blockStub.ping(Empty.newBuilder().build());
         logger.info("Successfully pinged the Blockstore server");
         
         // TODO: Implement your client here
+        Block b1 = stringToBlock("block_01");
+        Block b2 = stringToBlock("block_02");
+
+        ensure(blockStub.hasBlock(b1).getAnswer() == false);
+        ensure(blockStub.hasBlock(b2).getAnswer() == false);
+
+        blockStub.storeBlock(b1);
+        ensure(blockStub.hasBlock(b1).getAnswer() == true);
+
+        blockStub.storeBlock(b2);
+        ensure(blockStub.hasBlock(b2).getAnswer() == true);
+
+        Block b1prime = blockStub.getBlock(b1);
+        ensure(b1prime.getHash().equals(b1.getHash()));
+        ensure(b1prime.getData().equals(b1.getData()));
+
+        logger.info("We passed all the tests... yay!");
 	}
 
 	/*
---
+++ java/src/main/java/surfstore/HashUtils.java
@@ -0,0 +1,25 @@
+package surfstore;
+
+import java.nio.charset.StandardCharsets;
+        import java.security.MessageDigest;
+        import java.security.NoSuchAlgorithmException;
+        import java.util.Base64;
+
+public class HashUtils {
+
+    public static String sha256(String s) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
+
+        byte[] hash = digest.digest(s.getBytes(StandardCharsets.UTF_8));
+        String encoded = Base64.getEncoder().encodeToString(hash);
+
+        return encoded;
+    }
+
+}
